1a5dfa9419966ddaf1198cabf17c92b79ec498ce
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-09 15:31:07 -0700

    Add editing tools for ContentView. Add addNewCar.swift.

---
+++ listnav-demo/AddNewCar.swift
@@ -0,0 +1,69 @@
+//
+//  AddNewCar.swift
+//  listnav-demo
+//
+//  Created by user on 6/9/21.
+//
+
+import SwiftUI
+
+struct AddNewCar: View {
+    
+    @StateObject var carStore : CarStore
+    @State private var isHybrid = false
+    @State private var name: String = ""
+    @State private var description: String = ""
+
+    var body: some View {
+        Form {
+            Section(header: Text("Car Details")) {
+                Image(systemName: "car.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                DataInput(title: "Model", userInput: $name)
+                DataInput(title: "Description", userInput: $description)
+
+                Toggle(isOn: $isHybrid) {
+                        Text("Hybrid").font(.headline)
+                }.padding()
+            }
+            
+            Button(action: addNewCar) {
+                Text("Add Car")
+                }
+            }
+    }
+
+    func addNewCar() {
+        let newCar = Car(id: UUID().uuidString,
+                      name: name, description: description,
+                      isHybrid: isHybrid, imageName: "tesla_model_3" )
+        
+        carStore.cars.append(newCar)
+    }
+
+}
+
+struct DataInput: View {
+    
+    var title: String
+    @Binding var userInput: String
+    
+    var body: some View {
+        VStack(alignment: HorizontalAlignment.leading) {
+            Text(title)
+                .font(.headline)
+            TextField("Enter \(title)", text: $userInput)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        .padding()
+    }
+}
+
+struct AddNewCar_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewCar(carStore: CarStore(cars: carData))
+    }
+}
--- listnav-demo/ContentView.swift
+++ listnav-demo/ContentView.swift
@@ -12,14 +12,31 @@ struct ContentView: View {
     @StateObject var carStore: CarStore = CarStore(cars: carData)
     
     var body: some View {
-        
-    NavigationView {
-        List {
-            ForEach(carStore.cars) { car in
-                ListCell(car:car)
+        NavigationView {
+            List {
+                ForEach (carStore.cars) { car in
+                    ListCell(car: car)
+                }
+                .onDelete(perform: deleteItems)
+                .onMove(perform: moveItems)
                 
             }
+            .navigationBarTitle(Text("EV Cars"))
+            .navigationBarItems(leading: NavigationLink(destination:
+                                                            AddNewCar(carStore: self.carStore)) {
+                Text("Add")
+                    .foregroundColor(.blue)
+            }, trailing: EditButton())
         }
+        
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        carStore.cars.remove(atOffsets: offsets)
+    }
+    
+    func moveItems(from source: IndexSet, to destination: Int) {
+        carStore.cars.move(fromOffsets: source, toOffset: destination)
     }
 }
 
@@ -30,17 +47,18 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct ListCell: View {
+    
     var car: Car
+
     var body: some View {
         NavigationLink(destination: CarDetail(selectedCar: car)) {
-        HStack {
-            Image(car.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 60)
-            Text(car.name)
+            HStack {
+                Image(car.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 60)
+                Text(car.name)
+            }
         }
     }
-}
-}
 }
